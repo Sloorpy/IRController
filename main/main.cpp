29a25367fe3e383159d4e52ab0ff58806969e093
Alon <oved.alon@gmail.com>
2026-03-07 15:26:40 +0200

    Flashing tests

--- main/main.cpp
+++ main/main.cpp
@@ -5,6 +5,7 @@
 
 #include "Exception.hpp"
 #include "IRCommand.hpp"
+#include "IRReceiverIter.hpp"
 #include "driver/gpio.h"
 #include "esp_err.h"
 #include "freertos/FreeRTOS.h"
@@ -14,49 +15,40 @@
 
 #include <sys/time.h>
 
-static constexpr gpio_num_t IR_TRANSFER_PIN = GPIO_NUM_13;
+static constexpr gpio_num_t IR_RECV_PIN = GPIO_NUM_22;
+static constexpr gpio_num_t IR_SEND_PIN = GPIO_NUM_13;
 
-void turn_on_max(LEDTransmitter& led)
-{
-    led.send_multiple(LEDProtocol::POWER_ON, 30);
-
-    static constexpr uint16_t MAX_BRIGHTNESS = 12;
-    static constexpr uint16_t DELAY_BETWEEN_SIGNALS = 70;
-    led.send_multiple(LEDProtocol::BRIGHT_UP, MAX_BRIGHTNESS, DELAY_BETWEEN_SIGNALS);
+void receive_loop(IRReceiverIter& iter)
+{   
+    printf("Receiving is up\n");
+    while (true)
+    {
+        IRCommand cmd = iter.receive();
+        printf("Received command: %s\n", cmd.str().c_str());
+    }
 }
 
-void blink(LEDTransmitter& led)
+void send_loop(LEDTransmitter& transmitter)
 {
-    static constexpr uint64_t LED_BREAK_TIME_US = 10000;
-
-    led.send_multiple(LEDProtocol::WHITE_LIGHT, 2);
-    usleep(LED_BREAK_TIME_US);
-    
-    led.send_multiple(LEDProtocol::RED_LIGHT, 2);
-    usleep(LED_BREAK_TIME_US);
-    
-    led.send_multiple(LEDProtocol::GREEN_LIGHT, 2);
-    usleep(LED_BREAK_TIME_US);
-
-    led.send_multiple(LEDProtocol::YELLOW_LIGHT, 2);
-    usleep(LED_BREAK_TIME_US);
+    printf("Transmitting is up\n");
+    while (true)
+    {
+        transmitter.send(LEDProtocol::POWER_OFF);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
 }
+
 
 extern "C" void app_main(void)
 {
     try 
     {
-        LEDTransmitter ir_transmitter(IR_TRANSFER_PIN);
-                
+        IRReceiver recv(IR_RECV_PIN);
+        LEDTransmitter transmitter(IR_SEND_PIN);
         printf("IR Transmitter and Receiver initialized successfully\n");
         
-        turn_on_max(ir_transmitter);
-
-        while (true)
-        {
-            blink(ir_transmitter);
-        }
-    
+        send_loop(transmitter);
+        //receive_loop(recv.get_receiver());
     } 
     catch (EspException ex) {
         printf("ERROR: crashed with esp error code %d: %s\n", static_cast<uint16_t>(ex.get()), esp_err_to_name(ex.get()));
